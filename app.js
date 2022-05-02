2de37618157fc0f25f07d2c13e7778051c4548ae
gelomamo <angelfdezfdez87@gmail.com>
2022-05-02 19:23:02 +0000

    subir generar dominio

--- app.js
+++ app.js
@@ -2,18 +2,23 @@ let pronombre = ['el', 'nuestro' , 'vuestro' , 'mi'];
 let adjetivo = ['genial','gran','pequeño','pobre'];
 let nombre = ['circo','zoo','secreto','cotilleo'];
 let extension = ['.es','.com','.org'];
+let dominios = [] ;
+
 
 function generarDominio () {
     for (let con1=0; con1<pronombre.length ; con1++) {
         for (let con2=0; con2<adjetivo.length ; con2++) {
             for (let con3=0; con3<nombre.length ; con3++) {
                 for (let con4=0; con4<extension.length ; con4++) {
-                    console.log(pronombre[con1]+adjetivo[con2]+nombre[con3]+extension[con4]);
+                   dominios.push(pronombre[con1]+adjetivo[con2]+nombre[con3]+extension[con4]);
+                   console.log (dominios);
                 }
             }
         }
     }
-
+    return dominios;
 }
 
-generarDominio ();git remote set-url origin <your new url>
+let dominios_final = generarDominio ();
+let impresion = document.getElementById("imprimir");
+impresion.innerHTML = dominios_final;
--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let pronombre = ["el", "nuestro", "vuestro", "mi"];
+  let adjetivo = ["genial", "gran", "pequeño", "pobre"];
+  let nombre = ["circo", "zoo", "secreto", "cotilleo"];
+  let extension = [".es", ".com", ".org"];
+
+  function generarDominio() {
+    for (let con1 = 0; con1 < pronombre.length; con1++) {
+      for (let con2 = 0; con2 < adjetivo.length; con2++) {
+        for (let con3 = 0; con3 < nombre.length; con3++) {
+          for (let con4 = 0; con4 < extension.length; con4++) {
+            console.log(
+              pronombre[con1] + adjetivo[con2] + nombre[con3] + extension[con4]
+            );
+          }
+        }
+      }
+    }
+  }
+
+  generarDominio();
   console.log("Hello Rigo from the console!");
 };
